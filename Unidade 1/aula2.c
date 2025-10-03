99cfbe711b9583b142ad0082217d12c65749e358
gustavfaustino <gustav.faustino107@gmail.com>
2025-10-03 19:31:45 -0300

    Refatorado e adicionado comentários na aula2.c para clareza e contexto

--- Unidade 1/aula2.c
+++ Unidade 1/aula2.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-#include <locale.h>
+#include <stdio.h> //essa biblioteca é responsável pelas funções de entradas e saídas, como é o caso da função printf e scanf que vamos aprender mais à frente.
+#include <locale.h> // usada pra alterar o local do programa, como o idioma, pra formatar os acentos corretamente.
 
-int main() {
-    setlocale(LC_ALL, "pt_BR.UTF-8");
+int main() { //função principal do programa, onde a execução começa.
+    setlocale(LC_ALL, "pt_BR.UTF-8"); //função que altera o local do programa, nesse caso, para português do Brasil com suporte a UTF-8.
 
-    int x;
-    printf("Digite um número inteiro: ");
-    scanf("%d", &x);
-    printf("Olá, Mundo! Seu número, ó: %d", x);
-    return 0;
+    int x; //declaração de uma variável inteira chamada x.
+    printf("Digite um número inteiro: "); //função que imprime uma mensagem na tela pedindo ao usuário para digitar um número inteiro.
+    scanf("%d", &x); //função que lê a entrada do usuário e armazena o valor digitado na variável x. O & é usado para passar o endereço da variável.
+    printf("Olá, Mundo! Seu número, ó: %d", x); //função que imprime uma mensagem na tela, incluindo o valor da variável x usando o especificador de formato %d para inteiros.
+    return 0; //indica que o programa terminou com sucesso.
 }
